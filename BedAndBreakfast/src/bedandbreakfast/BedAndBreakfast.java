6451ca4345f57a1907c688592ddc40052341b554
skyorrichegg <Greg@192.168.0.9>
2016-03-29 21:25:16 -0700

    I added my message to this file

--- BedAndBreakfast/src/bedandbreakfast/BedAndBreakfast.java
+++ BedAndBreakfast/src/bedandbreakfast/BedAndBreakfast.java
@@ -17,6 +17,7 @@ public class BedAndBreakfast {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
+        System.out.println("Hello World Again! <-this was added by Greg");
     }
     
 }
