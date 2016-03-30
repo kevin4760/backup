94df385d3712245723826b12f80bea09178459f3
skyorrichegg <gregmhicks@yahoo.com>
2016-03-30 00:49:52 -0700

    Update BedAndBreakfast.java

--- BedAndBreakfast/src/bedandbreakfast/BedAndBreakfast.java
+++ BedAndBreakfast/src/bedandbreakfast/BedAndBreakfast.java
@@ -17,7 +17,7 @@ public class BedAndBreakfast {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.println("Hello World!");
-        System.out.println("new addition!");
+        System.out.println("new additionsssss!");
     }
     
 }
